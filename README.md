ba720eae4e2fbc32d880fb3d24c9fee231b7765e
sndnvaps <sndnvaps@gmail.com>
2017-01-25 01:23:34 +0800

    init yahoo weather api
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# Yahoo Query Language (YQL)
+
+	go get github.com/sndnvaps/yahoo_weather_api
+
+```
+type ForecastInfo struct {
+	Code float64
+	Date string //time.Time
+	Day  string // Tue,Wed,Thu,Fri,Sat,Sun
+	High float64
+	Low  float64
+	Text string
+}
+```
+
+# example
+
+```go
+package main
+
+import (
+	"github.com/sndnvaps/yahoo_weather_api"
+	"fmt"
+)
+
+func main() {
+
+	f := yahoo.GetForecastlData("jiangmen,guangdong,china")
+	fmt.Println(f)
+}
+```
+
+output:
+
+```json
+	[{30 25 Jan 2017 Wed 71 56 Partly Cloudy} {32 26 Jan 2017 Thu 71 52 Sunny} {34 27 Jan 2017 Fri 71 51 Mostly Sunny} {30 28 Jan 2017 Sat 72 55 Partly Cloudy} {28 29 Jan 2017 Sun 72 62 Mostly Cloudy} {28 30 Jan 2017 Mon 73 64 Mostly Cloudy} {28 31 Jan 2017 Tue 71 59 Mostly Cloudy} {30 01 Feb 2017 Wed 74 60 Partly Cloudy} {30 02 Feb 2017 Thu 73 60 Partly Cloudy} {30 03 Feb 2017 Fri 70 61 Partly Cloudy}]
+```
---
+++ yahoo_weather_api.go
@@ -0,0 +1,116 @@
+// use Yahoo Query Language (YQL)
+package yahoo
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/koron/go-dproxy"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+/*
+select woeid from geo.places where text="jiangmen,guangdong,china"
+
+
+select * from weather.forecast where woeid in  (select woeid from geo.places where text="jiangmen,guangdong,china")
+*/
+
+const (
+	publicAPIURL string = "http://query.yahooapis.com/v1/public/yql"
+)
+
+type ForecastInfo struct {
+	Code float64
+	Date string //time.Time
+	Day  string // Tue,Wed,Thu,Fri,Sat,Sun
+	High float64
+	Low  float64
+	Text string
+}
+
+// runQuery runs the query and retuns the
+// results in an io.Reader
+func RunQuery(query string) []byte {
+	queryURL := BuildURL(query)
+	return getJSON(queryURL)
+}
+
+//buildQuery creates a YQL sql query
+// select * from weather.forecast where woeid in  (select woeid from geo.places where text="jiangmen,guangdong,china")
+//location == "jiangmen,guangdong,china or
+//location == "jiangmen,guangdong
+func BuildQuery(location string) string {
+	query := fmt.Sprintf(`select * from weather.forecast where woeid in  (select woeid from geo.places where text="%s")`, location)
+	return query
+}
+
+// buildURL creates a YQL URL from a query
+func BuildURL(query string) string {
+	params := url.Values{}
+	params.Add("q", query)
+	params.Add("format", "json")
+	//	params.Add("diagnostics", "true")
+	params.Add("callback", "")
+
+	return publicAPIURL + "?" + params.Encode()
+}
+
+// getJSON returns the JSON response
+// from a request to a given URL
+func getJSON(url string) []byte {
+	resp, errReq := http.Get(url)
+
+	if errReq != nil {
+		log.Fatalf("%s", errReq.Error())
+	}
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return body //bytes.NewReader(body)
+}
+
+func GetForecastlData(location string) []ForecastInfo {
+	var Forecasts []ForecastInfo
+	var v interface{}
+	query := BuildQuery(location)
+	jsr := RunQuery(query)
+	json.Unmarshal([]byte(jsr), &v)
+	forecasts := dproxy.New(v).M("query").M("results").M("channel").M("item").M("forecast") //.A(0).M("day").String()
+	for i := 0; i < 10; i++ {
+		//f := forecasts.A(i)
+		Forecasts = append(Forecasts, newForecastDataFromRow(forecasts.A(i)))
+	}
+	return Forecasts
+
+}
+
+// newHistoricalPieceFromRow returns a
+func newForecastDataFromRow(row dproxy.Proxy) ForecastInfo {
+	f := ForecastInfo{}
+
+	code, _ := row.M("code").String()
+	f.Code, _ = strconv.ParseFloat(code, 64)
+
+	date, _ := row.M("date").String()
+	//for debug
+	//fmt.Println(date)
+	f.Date = date
+	//f.Date, _ =  time.Parse("2006-01-02", date)
+
+	f.Day, _ = row.M("day").String() //
+
+	high, _ := row.M("high").String()
+	f.High, _ = strconv.ParseFloat(high, 64)
+
+	low, _ := row.M("low").String()
+	f.Low, _ = strconv.ParseFloat(low, 64)
+
+	f.Text, _ = row.M("text").String()
+
+	return f
+}
