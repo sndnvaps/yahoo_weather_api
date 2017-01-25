bce46fa3a44a2255cdbfc973de3d5f08f2bff7bc
sndnvaps <sndnvaps@gmail.com>
2017-01-25 13:06:58 +0800

    update add Fahrenheit2Celsius func
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- yahoo_weather_api.go
+++ yahoo_weather_api.go
@@ -9,8 +9,8 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 )
-
 
 const (
 	publicAPIURL string = "http://query.yahooapis.com/v1/public/yql"
@@ -23,6 +23,32 @@ type ForecastInfo struct {
 	High float64
 	Low  float64
 	Text string
+}
+
+type WindInfo struct {
+	Chill     float64
+	Direction float64
+	Speed     float64
+}
+
+type Atmosphere struct {
+	Humidity   float64
+	Pressure   float64
+	Rising     float64
+	Visibility float64
+}
+
+type Image struct {
+	url string
+}
+
+type Conditions struct {
+	Title string
+	Lat   float64
+	Long  float64
+	Date  time.Time
+	temp  float64
+	Text  string
 }
 
 // runQuery runs the query and retuns the
@@ -67,20 +93,18 @@ func getJSON(url string) []byte {
 	return body //bytes.NewReader(body)
 }
 
-func GetForecastlData(location string) []ForecastInfo {
+func GetForecastlData(location string) ([]ForecastInfo, string) {
 	var Forecasts []ForecastInfo
-	var v interface{}
-	query := BuildQuery(location)
-	jsr := RunQuery(query)
-	json.Unmarshal([]byte(jsr), &v)
-	forecasts := dproxy.New(v).M("query").M("results").M("channel").M("item").M("forecast") //.A(0).M("day").String()
 
+	ChannelNode := GetChannelNode(location)
+	forecasts := ChannelNode.M("item").M("forecast")
 	fca, _ := forecasts.Array()
 	for i := 0; i < len(fca); i++ {
 		//f := forecasts.A(i)
 		Forecasts = append(Forecasts, newForecastDataFromRow(forecasts.A(i)))
 	}
-	return Forecasts
+	icon_url := GetWeatherIcon(ChannelNode)
+	return Forecasts, icon_url
 
 }
 
@@ -100,12 +124,53 @@ func newForecastDataFromRow(row dproxy.Proxy) ForecastInfo {
 	f.Day, _ = row.M("day").String() //
 
 	high, _ := row.M("high").String()
-	f.High, _ = strconv.ParseFloat(high, 64)
+	High, _ := strconv.ParseFloat(high, 64)
+	f.High = Fahrenheit2Celsius(High)//High 
 
 	low, _ := row.M("low").String()
-	f.Low, _ = strconv.ParseFloat(low, 64)
+	Low, _ := strconv.ParseFloat(low, 64)
+	f.Low =  Fahrenheit2Celsius(Low)//Low
 
 	f.Text, _ = row.M("text").String()
 
 	return f
+}
+
+func GetChannelNode(location string) dproxy.Proxy {
+	var v interface{}
+	query := BuildQuery(location)
+	jsr := RunQuery(query)
+	json.Unmarshal([]byte(jsr), &v)
+	channel := dproxy.New(v).M("query").M("results").M("channel")
+	return channel
+}
+
+//get the icon of weather yahoo
+// i := GetChannelNode(location)
+func GetWeatherIcon(i dproxy.Proxy) string {
+	icon, _ := i.M("image").M("url").String()
+	return icon
+}
+
+//get the wind info
+//w := GetChannelNode(location)
+func GetWindInfo(w dproxy.Proxy) WindInfo {
+	wind := WindInfo{}
+	wi := w.M("wind")
+
+	chill, _ := wi.M("chill").String()
+	wind.Chill, _ = strconv.ParseFloat(chill, 64)
+
+	direction, _ := wi.M("direction").String()
+	wind.Direction, _ = strconv.ParseFloat(direction, 64)
+
+	speed, _ := wi.M("speed").String()
+	wind.Speed, _ = strconv.ParseFloat(speed, 64)
+
+	return wind
+}
+
+//℃
+func Fahrenheit2Celsius(f float64) float64 {
+	return ((f - 32) / 1.8)
 }
